1077995015d883ec6f99fcabca4c0c58141142be
FlaviaBastos <flasanches@gmail.com>
2020-03-21 12:29:33 -0400

    Add timer and wait to start

--- quiz.go
+++ quiz.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"time"
 )
 
 // Line holds each line from file
@@ -16,7 +18,9 @@ type Line struct {
 
 func main() {
 	var filename string
+	var timer string
 	flag.StringVar(&filename, "csv", "questions.csv", "A csv file with one question and answer per line")
+	flag.StringVar(&timer, "time", "30", "Quiz time limit in seconds")
 	flag.Parse()
 
 	// Open file
@@ -38,10 +42,52 @@ func main() {
 		os.Exit(1)
 	}
 
+	// creates quiz from file
 	quiz := parse(lines)
 
+	// determine time cap
+	t, err := strconv.Atoi(timer)
+	if err != nil {
+		fmt.Println("this is not a valid timeout value", err)
+	}
+	timelimit := time.Duration(t) * time.Second
+
+	// get ready to start game
+	fmt.Println("Ready? Hit <ENTER> to start!")
+	fmt.Scanln()
+
+	// start game
+	c := make(chan int)
+	go runQuiz(quiz, c)
+	timeout := time.After(timelimit)
+
+	// wait for either quiz to finish or time runs out
+	for {
+		select {
+		case total := <-c:
+			fmt.Printf("Your score is: %v out of %v\n", total, len(quiz))
+			return
+		case <-timeout:
+			fmt.Println("Time is up!")
+			return
+		}
+	}
+}
+
+func parse(lines [][]string) []Line {
+	// Saves file content into struct
+	ques := make([]Line, len(lines)) // using make instead of append because the length is known, so no need to adjust size everytime
+	for i, line := range lines {
+		ques[i] = Line{
+			Question: line[0],
+			Answer:   line[1],
+		}
+	}
+	return ques
+}
+
+func runQuiz(quiz []Line, c chan int) {
 	var score int
-	fmt.Println("Ready? Go!")
 	for _, question := range quiz {
 		var input string
 		fmt.Printf("Question: %v\n", question.Question)
@@ -58,20 +104,6 @@ func main() {
 		} else {
 			fmt.Println("Wrong")
 		}
-
 	}
-
-	fmt.Printf("Your score is: %v out of %v\n", score, len(quiz))
-}
-
-func parse(lines [][]string) []Line {
-	// Saves file content into struct
-	ques := make([]Line, len(lines)) // using make instead of append because the length is known, so no need to adjust size everytime
-	for i, line := range lines {
-		ques[i] = Line{
-			Question: line[0],
-			Answer:   line[1],
-		}
-	}
-	return ques
+	c <- score
 }
